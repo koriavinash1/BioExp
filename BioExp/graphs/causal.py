4d7078b8170ba25a06b4723ff6b9fdd0e44324f0
koriavinash1 <koriavinash1@gmail.com>
2020-02-18 19:00:05 +0530

    causal update

--- BioExp/graphs/causal.py
+++ BioExp/graphs/causal.py
@@ -54,64 +54,44 @@ class CausalGraph():
 				return idx
 		
 
-	def get_link(self, nodeA_info, nodeB_info, dataset_path, loader, save_path, max_samples = 1):
+	def get_link(self, nodeA_info, nodeB_info, dataset_path, loader, max_samples = 1):
 		"""
 			get link between two nodes, nodeA, nodeB
 			occlude at nodeA and observe changes in nodeB
 			nodeA_info    : {'layer_name', 'layer_idxs'}
 			nodeB_info    : {'layer_name', 'layer_idxs'}
 		"""
-		self.modelcopy.load_weights(self.weights, by_name = True)
 		self.model.load_weights(self.weights, by_name = True)
 
 		nodeA_idx   = self.get_layer_idx(nodeA_info['layer_name'])
 		nodeA_idxs  = nodeA_info['layer_idxs']
 		nodeB_idx   = self.get_layer_idx(nodeB_info['layer_name'])
 		nodeB_idxs  = nodeB_info['layer_idxs']
+                
+                total_filters = np.arange(np.array(self.model.layers[nodeB_idx].get_weights())[0].shape[-1])
+                test_filters  = np.delete(total_filters, nodeB_idxs)
+
+                layer_weights = np.array(self.model.layers[node_idx].get_weights().copy())
+                occluded_weights = layer_weights.copy()
+                for j in test_filters:
+                        occluded_weights[0][:,:,:,j] = 0
+                        try:
+                                occluded_weights[1][j] = 0
+                        except: pass
+
+                self.model.layers[nodeB_idx].set_weights(occluded_weights)
+                model = Model(inputs = self.model.input, outputs=self.model.get_layer(nodeB_info['layer_name']).output)
 
 
-		layer_weights = np.array(self.modelcopy.layers[nodeA_idx].get_weights())
-		occluded_weights = layer_weights.copy()
-		for j in nodeA_idxs:
-			occluded_weights[0][:,:,:,j] = 0
-			occluded_weights[1][j] = 0
-		self.modelcopy.layers[nodeA_idx].set_weights(occluded_weights)
-
-		layer_weights = np.array(self.modelcopy.layers[nodeB_idx].get_weights())
-		occluded_weights = layer_weights.copy()
-
-		for j in nodeB_idxs:
-			occluded_weights[0][:,:,:,j] = 0
-			occluded_weights[1][j] = 0
-		self.modelcopy.layers[nodeB_idx].set_weights(occluded_weights)
-
-		dice_json = {}
-		for class_ in self.classinfo.keys():
-			dice_json[class_] = []
-
+                true_distributionB = []
 		input_paths = os.listdir(dataset_path)
 		for i in range(len(input_paths) if len(input_paths) < max_samples else max_samples):
 			input_, label_ = loader(os.path.join(dataset_path, input_paths[i]), 
 								os.path.join(dataset_path, 
 								input_paths[i]).replace('mask', 'label').replace('labels', 'masks'))
-			prediction_occluded = np.squeeze(self.modelcopy.predict(input_[None, ...]))
 			prediction = np.squeeze(self.model.predict(input_[None, ...]))
 
-			idx = 0
-			if self.noutputs > 1:
-				for ii in range(self.noutputs):
-					if prediction[ii] == self.nclasses:
-						idx = ii 
-						break;
 
-
-			for class_ in self.classinfo.keys():
-				if self.noutputs > 1:
-					dice_json[class_].append(self.metric(label_, prediction[idx].argmax(axis = -1), self.classinfo[class_]) - 
-								self.metric(label_, prediction_occluded[idx].argmax(axis = -1), self.classinfo[class_]))
-				else:
-					dice_json[class_].append(self.metric(label_, prediction.argmax(axis = -1), self.classinfo[class_]) - 
-								self.metric(label_, prediction_occluded.argmax(axis = -1), self.classinfo[class_]))
 
 
 		for class_ in self.classinfo.keys():
@@ -120,10 +100,9 @@ class CausalGraph():
 		return dice_json
 
 
-	def generate_graph(self, graph_info):
+	def generate_graph(self, graph_info, dataset_path, dataset_path, dataloader, save_path = None, verbose = False, max_samples=10):
 		layers   = graph_info['layer_name']
 		concept_names = graph_info['concept_name']
-                concept_distribution = graph_info['conecpt_distribution']
 		filter_idxs   = graph_info['feature_map_idxs']
 
 		layer_names  = np.unique(layers)
@@ -139,15 +118,16 @@ class CausalGraph():
                 node_indexing = np.array(node_indexing)
                 node_ordering = np.array(node_ordering)
 
-
-                for idxi, nodei in zip(node_indexing, node_ordering):
-                        idistribution = concept_distribution[concept_names == nodei][0]
-                        for iprobability in [0, 1]:
-                                for idxj, nodej in zip(node_indexing[node_indexing >= idxi], node_ordering[node_indexing >= idx]):
-                                        jdistribution = concept_distribution[concept_names == nodej][0]
-                                        for jprobability in [0, 1]:
-                                                ## link formation
-
+                self.causal_graph = np.zeros((len(node_ordering), len(node_ordering))).astype('int')
+                for ii, (idxi, nodei) in enumerate(zip(node_indexing, node_ordering)):
+                        nodei_info = {'concept_name': nodei, 'layer_name': layers[concept_names == nodei], 'feature_map_idxs': filter_idxs[concept_names == nodei]}
+                        for jj, (idxj, nodej) in enumerate(zip(node_indexing[node_indexing > idxi], node_ordering[node_indexing > idx])):
+                                nodej_info = {'concept_name': nodej, 'layer_name': layers[concept_names == nodej], 'feature_map_idxs': filter_idxs[concept_names == nodei]}
+                                self.causal_graph[ii, jj] = self.get_link(nodei_info,
+                                                                            nodej_info,
+                                                                            dataset_path = dataset_path,
+                                                                            loader = dataloader,
+                                                                            max_samples = max_samples)
 
 		pass
 
